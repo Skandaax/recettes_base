74f72dc97ad24ce3c86e8f007f51f6ac074cf965
Yannick Couillin <Inforaz-admin@INFORAZ.local>
2020-03-23 10:08:51 +0100

    modification fichier menu.php

--- html/menu.php
+++ html/menu.php
@@ -6,6 +6,8 @@
 
 <!DOCTYPE html>
 <html lang="fr">
+
+
 <head >
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1"/>
@@ -20,19 +22,19 @@
             <li><a href ="#">Page d'accueil</a></li>
             <li><a href ="index?page=entree">Les entrées</a>
                 <ul class="sous-menu">
-                    <li><a href ="index.php?page=entree&page=entree1">Entrée 1</a></li>
-                    <li><a href ="index.php?page=entree&page=entree2">Entrée 2</a></li>
-                    <li><a href ="index.php?page=entree&page=entree3">Entrée 3</a></li>
+                    <li><a href ="recettes/entrees/entree1.html">Entrée 1</a></li>
+                    <li><a href ="recettes/entrees/entree2.html">Entrée 2</a></li>
+                    <li><a href ="recettes/entrees/entree3.html">Entrée 3</a></li>
                 </ul>
             </li>
-            <li><a href ="index.php?page=entree">Les plats</a>
+            <li><a href ="index.php?page=plat">Les plats</a>
                 <ul class="sous-menu">
                     <li><a href ="recettes/plats/plat1.html">Plat 1</a></li>
                     <li><a href ="recettes/plats/plat2.html">Plat 2</a></li>
                     <li><a href ="recettes/plats/plat3.html">Plat 3</a></li>
                 </ul>
             </li>
-            <li><a href ="index.php?page=desserts">Les desserts</a>
+            <li><a href ="index.php?page=dessert">Les desserts</a>
                 <ul class="sous-menu">
                     <li><a href ="recettes/desserts/dessert1.html">Dessert 1</a></li>
                     <li><a href ="recettes/desserts/dessert2.html">Dessert 2</a></li>
