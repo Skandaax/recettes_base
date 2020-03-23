1bd4bd35db5743cc47d0835939a378b3eecb23b0
Yannick Couillin <Inforaz-admin@INFORAZ.local>
2020-03-23 16:45:51 +0100

    modification php

--- html/recettes.php
+++ html/recettes.php
@@ -3,12 +3,18 @@
 
 $recetes_test = array(
     'titre' => 'entrée 1',
-    'image' => '../img/entree1.jpg',
+    'image' => 'img\dessert1.jpg',
+    'titre1' => 'Temps de préparation',
     'Temps de préparation' => '15sec',
+    'titre2' => 'Personnes',
     'personnes' => '8',
+    'Titre3' => 'Difficulté',
     'Difficulté' => '1/4',
+    'Titre4' => 'Coût',
     'Coût' => '1/4',
+    'titre5' => 'Ingrédients',
     'Ingrédients' => '1 paquet depâtes de lasagnes///3 oignons jaunes///3 gousses d ail//1 branche de celerie///1carotte',
+    'titre6' => 'Etapes',
     'Etapes' => 'Faire revenir gousses hachées d ail et les oignons émincés dans un peu d huile d olive.///Ajouter la carrotte et la branche de céleri hachée puis la viande et faire revenir le tout.///Au bout de quelques minutes, ajouter le vin rouge. Laisser cuire jusqu a évaporation.'
 );
 
@@ -33,7 +39,19 @@ var_dump($recetes_test);
     </header>
 <body>
     <h1><?php echo $recetes_test['titre']?></h1>
-    <div></div>
+    <div><?php ?></div>
+    <div>
+        <tr>
+            <td><?php echo $recetes_test['titre1']?></td>
+            <td><?php echo $recetes_test['Temps de préparation']?></td>
+        </tr>
+        <tr>
+            <td><?php echo $recetes_test['titre1']?></td>
+            <td><?php echo $recetes_test['Temps de préparation']?></td>
+        </tr> 
+
+    </div>
+
 </body>
 
 </html>
