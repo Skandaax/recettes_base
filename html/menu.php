bf31b8da174f73259e440eca758a99459d62ba07
Yannick Couillin <Inforaz-admin@INFORAZ.local>
2020-03-23 09:54:03 +0100

    Modification fichier menu.php

--- html/menu.php
+++ html/menu.php
@@ -1,24 +1,38 @@
-include
+<?php
+
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="fr">
+<head >
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1"/>
+    <title>Devwbmob</title>
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+
+</head>
 
 <header>
     <nav class="menu">
         <ul>
-            <li><a href ="index.php">Page d'accueil</a></li>
+            <li><a href ="#">Page d'accueil</a></li>
             <li><a href ="index?page=entree">Les entrées</a>
                 <ul class="sous-menu">
-                    <li><a href ="index.php?page=entree1">Entrée 1</a></li>
-                    <li><a href ="recettes/entrees/entree2.html">Entrée 2</a></li>
-                    <li><a href ="recettes/entrees/entree3.html">Entrée 3</a></li>
+                    <li><a href ="index.php?page=entree&page=entree1">Entrée 1</a></li>
+                    <li><a href ="index.php?page=entree&page=entree2">Entrée 2</a></li>
+                    <li><a href ="index.php?page=entree&page=entree3">Entrée 3</a></li>
                 </ul>
             </li>
-            <li><a href ="recettes/plat.html">Les plats</a>
+            <li><a href ="index.php?page=entree">Les plats</a>
                 <ul class="sous-menu">
                     <li><a href ="recettes/plats/plat1.html">Plat 1</a></li>
                     <li><a href ="recettes/plats/plat2.html">Plat 2</a></li>
                     <li><a href ="recettes/plats/plat3.html">Plat 3</a></li>
                 </ul>
             </li>
-            <li><a href ="recettes/dessert.html">Les desserts</a>
+            <li><a href ="index.php?page=desserts">Les desserts</a>
                 <ul class="sous-menu">
                     <li><a href ="recettes/desserts/dessert1.html">Dessert 1</a></li>
                     <li><a href ="recettes/desserts/dessert2.html">Dessert 2</a></li>
--- index.php
+++ index.php
@@ -1,10 +1,16 @@
 <?php
 
 $page = "entree";
+$page2 = "plats";
+$page3 = "desserts";
+
 
 var_dump($page);
 
  
+
+
+
 
 require "html/menu.php";
 
