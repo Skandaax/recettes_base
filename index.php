0bd64643c81671c94a62d60412f60440f450aa99
Yannick Couillin <Inforaz-admin@INFORAZ.local>
2020-03-23 11:10:55 +0100

    creation d'un fichier accueil.php

--- index.php
+++ index.php
@@ -3,7 +3,28 @@
 
 var_dump($_GET);
 
+$page = (isset($_GET["page"]))? $_GET{"page"} : "entree";// entree
+
+switch ($page){
+    case "home":
+        echo "Vous avez demandé la page d'accueil !";
+        break;
+    case "entree":
+        echo "Page entree est ouverte";
+        break;
+    case "plat":
+        echo "Page plat est ouverte";
+        break;
+    case "dessert":
+        echo "page dessert est ouverte";
+        break;
+    default : echo "la page d'accueil s'ouvre !";
+}
+
+
+
 require "html/menu.php";
+
 
 ?>
  
@@ -15,25 +36,13 @@ require "html/menu.php";
         <meta charset="utf-8">
          <link rel="stylesheet" type="text/css" href="css/style.css">
     </head>
-    
+
     <body>
         <header>
         </header>
         
-        <article>
-            <h2 class="text-center">Suggestion du jour</h2>
-            <ul class="row items">
-                <li class="img-bullet col-4 text-center"><img src="img/entree3.jpg" alt="Photo de l'entrée 3"><br><a href ="recettes/entrees/entree3.html">Entrée 3</a></li>
-                <li class="img-bullet col-4 text-center"><img src="img/plat1.jpg" alt="Photo du plat 1"><br><a href ="recettes/plats/plat1.html">Plat 1</a></li>
-                <li class="img-bullet col-4 text-center"><img src="img/dessert1.jpg" alt="Photo du dessert 1"><br><a href ="recettes/desserts/dessert1.html">Dessert 1</a></li>
-            </ul>
-        </article>
+        <?php require "html/accueil.php" ?>
+
     </body>
 
 </html>
-
-
-
-
-
-
